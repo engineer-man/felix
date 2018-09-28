f639f990ae08ab7245b966bd78237195477a9d2f
brtwrst <6873862+brtwrst@users.noreply.github.com>
2018-09-28 23:57:51 +0200

    hangman kind of working

--- python/bot.py
+++ python/bot.py
@@ -1,126 +1,39 @@
 import discord
 import json
-import hangman.hangman as hm
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-bot = Bot(command_prefix='~ ', description='None')
+bot = Bot(command_prefix=('felix ','+ '), description='None')
 config = json.load(open("../config.json", "r"))
 
-STARTUP_EXTENSIONS = ['cogs.duckresponse']
+STARTUP_EXTENSIONS = ['cogs.duckresponse.duckresponse',
+                      'cogs.inviteblocker',
+                      'cogs.hangman.hangman']
 for extension in STARTUP_EXTENSIONS:
-        try:
-            bot.load_extension(f'{extension}')
-        except Exception as e:
-            exc = f'{type(e).__name__}: {e}'
-            print(f'Failed to load extension {extension}\n{exc}')
+	try:
+		bot.load_extension(f'{extension}')
+	except Exception as e:
+		exc = f'{type(e).__name__}: {e}'
+		print(f'Failed to load extension {extension}\n{exc}')
+
 
 @bot.event
 async def on_ready():
-	# This is just for debugging
-	app_info = await bot.application_info()
-	await app_info.owner.send(
-		'Ready\n'
-		+ f'```css\nLoaded extensions:'
-		+ f' {[e for e in bot.extensions]}```')
-	return True
+    # This is just for debugging
+    app_info = await bot.application_info()
+    await app_info.owner.send(
+        'Ready\n'
+        + f'```css\nLoaded extensions:'
+        + f' {[e for e in bot.extensions]}```')
+    return True
+
 
 @bot.event
 async def on_message(message):
-	if message.content in '++':
-		await bot.close()
-	await bot.process_commands(message)
-
-# @bot.command(pass_context=True)
-# async def hangman(ctx):
-# 	hidden = hm.hangman(ctx.message.server.id, ctx.message.channel.name)
-# 	if hidden == hm.GAMEACTIVE:
-# 		await bot.say("A game is already active in this chat.")
-# 		return
-# 	embed = discord.Embed(title="Duckie hangman",
-# 			description="Thank you for playing Duckie hang man, inspired by akaeddy#3508\n"\
-# 			+ "your word is: " + " ".join(hidden) + "\nyou have 6 mess ups\n\npotential"\
-# 				+ " points for this word: %d" % (hm.getPotPoints(ctx.message.server.id, ctx.message.channel.name)),
-# 			 color=0x801680)
-# 	await bot.say(embed=embed)
-
-# @bot.command(pass_context=True)
-# async def letter(ctx):
-# 	letter = ""
-# 	try:
-# 		letter = ctx.message.content.split(" ")[2][0].lower()
-# 	except Exception as e:
-# 		await bot.say("quaaaack quack?")
-# 		return
-# 	err = hm.play(ctx.message.server.id, ctx.message.channel.name, letter,
-# 					ctx.message.author.id)
-# 	if err < 0:
-# 		if err == hm.LOSTGAME:
-# 			await bot.say("You've lost, the word was " + hm.getWord(ctx.message.server.id,\
-# 				ctx.message.channel.name))
-# 		if err == hm.ERRORNOGAME:
-# 			await bot.say("you need to start a game quaaaack.")
-# 		if err == hm.ERRORGAMECOMP:
-# 			reply = "already solved; the word was " + hm.getWord(ctx.message.server.id, ctx.message.channel.name)
-# 			await bot.say(reply)
-# 		if err == hm.ERRORINVALID:
-# 			await bot.say("quaaaack quack? quack")
-# 		if err == hm.ERRORDUP:
-# 			await bot.say("You've already tried that letter!! Quack")
-# 		return
-# 	elif err == hm.WONGAME:
-# 		await bot.say("Nice you won!!! The word was " + hm.getWord(ctx.message.server.id,\
-# 			ctx.message.channel.name))
-# 		return
+    if message.content in '~ quit':
+        await bot.close()
+    await bot.process_commands(message)
 
 
-# 	guessesr = hm.getRightLetters(ctx.message.server.id, ctx.message.channel.name)
-# 	guessesw = hm.getWrongLetters(ctx.message.server.id, ctx.message.channel.name)
-# 	tries = hm.getTries(ctx.message.server.id, ctx.message.channel.name)
-# 	guess = hm.getGuess(ctx.message.server.id, ctx.message.channel.name)
-
-# 	embed = discord.Embed(title="Duckie hangman",
-# 		description="your word is: " + " ".join(guess) + "\nyou have "\
-# 		+ "%d" % (tries) + " mess ups\n\n"\
-# 		+ "incorrect: " + guessesw + "\n"\
-# 		+ "correct: " + guessesr ,
-# 		color=0x801680)
-# 	await bot.say(embed=embed)
-
-# @bot.command(pass_context=True, description="")
-# async def highscores(ctx):
-# 	names = hm.topten(ctx.message.server.id)
-# 	embed = discord.Embed(title="Duckie hangman", description="Highscores, top ten values!", color=0x161680)
-
-# 	if (len(names) == 0):
-# 		await bot.say("There are no highscores to display")
-# 		return;
-
-# 	namesdata = ""
-# 	scoresdata = ""
-# 	i = 1
-# 	for n in reversed(names):
-# 		username = ctx.message.server.get_member(n[0])
-# 		if username != None:
-# 			usernamestr = "%s#%s" % (username.name, username.discriminator)
-# 			namesdata = namesdata + ("%d) " % (i) + usernamestr) + "\n"
-# 			scoresdata = scoresdata + "\t%d\n" % n[1]
-# 			if i == 10:
-# 				break
-# 			i = i + 1
-# 		else:
-# 			print("skipping: %s" % (n[0]))
-
-# 	embed.add_field(name="score", value=scoresdata, inline=True)
-# 	embed.add_field(name="users", value=namesdata, inline=True)
-# 	await bot.say(embed=embed)
-
-# @bot.command(pass_context=True, description="")
-# async def myscore(ctx):
-# 	embed = discord.Embed(title="Duckie hangman", description="your score is... "
-# 		+ "%d!" % (hm.myscore(ctx.message.server.id, ctx.message.author.id)), color=0x161680)
-# 	await bot.say(embed=embed)
-
-
-hm.loaddata()
 bot.run(config["bot_key"])
+pass
---
+++ python/cogs/inviteblocker.py
@@ -0,0 +1,77 @@
+"""This is a cog for a discord.py bot.
+It will auto delete messages that contain discord invite links
+The allow command will allow the specified user to post 1 link
+
+Load the cog by calling client.load_extension with the name of this python file
+as an argument (without the file-type extension)
+    example:    bot.load_extension('inviteblocker')
+or by calling it with the path and the name of this python file
+    example:    bot.load_extension('my_extensions.inviteblocker')
+
+Only people belonging to a group that is specified in the .allowed file
+can use the commands.
+"""
+
+from discord.ext import commands
+from discord import TextChannel, Member
+import re
+
+
+class InviteBlocker():
+    def __init__(self, client):
+        self.client = client
+        self.allowed = []
+        # Load id's of roles that are allowed to use commands from this cog
+        with open(__file__.replace('.py', '.allowed')) as f:
+            self.command_enabled_roles = [
+                int(id) for id in f.read().strip().split('\n')
+            ]
+
+    # __local_command() is run on every command
+    # Checks if a user is allowed to call the command
+    async def __local_check(self, ctx):
+        # Always allow bot owner
+        if await ctx.bot.is_owner(ctx.author):
+            return True
+        try:
+            user_roles = [role.id for role in ctx.message.author.roles]
+        except AttributeError:
+            return False
+
+        return any(role in self.command_enabled_roles for role in user_roles)
+
+    # ----------------------------------------------
+    # Event listeners
+    # ----------------------------------------------
+    async def on_message(self, msg):
+        # Ignore the bot's own messages
+        if msg.author.bot:
+            return
+        if isinstance(msg.channel, TextChannel):  # (don't run in DM Channels)
+            if len(re.findall(r'(discord\.(gg|io|me)\/\S+)', msg.content)) > 0:
+                if msg.author.id in self.allowed:
+                    self.allowed.remove(msg.author.id)
+                else:
+                    await msg.channel.send(
+                        f'Sorry {msg.author.mention}. ' +
+                        'Posting Links to other servers is not allowed.\n'+
+                        'You can ask permission from an admin or moderator!'
+                    )
+                    await msg.delete()
+
+    # ----------------------------------------------
+    # Method to allow 1 discord.gg link
+    # ----------------------------------------------
+    @commands.command(
+        name='allow',
+        brief='Allow a single discord.gg link.',
+        description='Allow a single discord.gg link.',
+    )
+    @commands.guild_only()
+    async def allow(self, ctx, member: Member):
+        await ctx.send(f'Hey {member.mention}, you can post 1 discord.gg link!')
+        self.allowed.append(member.id)
+
+
+def setup(client):
+    client.add_cog(InviteBlocker(client))
