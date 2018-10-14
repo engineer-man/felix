13b06248aa3e1756c5a82191b72b13f87dfe2b38
brtwrst <6873862+brtwrst@users.noreply.github.com>
2018-10-14 09:11:59 +0200

    Some cleanup before merging.
    Added questions.txt to gitignore.
    Changed bot to client.
    Added command description.
    Make command hidden.

--- python/cogs/insults.py
+++ python/cogs/insults.py
@@ -7,16 +7,16 @@ import json
 questions = open('questions.txt').read().splitlines()
 
 def is_cc_channel():
-    """Used as a decorator for bot commands
-    to make sure only staff can see/use it
+    """Used as a decorator for bot commands to make them
+    only useable in the em/command center channel
     """
     async def predicate(ctx):
         return ctx.channel.id == 485581363383107604
     return commands.check(predicate)
 
-class insults:
-    def __init__(self, bot):
-        self.bot = bot
+class Insults:
+    def __init__(self, client):
+        self.client = client
         with open(path.join(path.dirname(__file__), 'permissions.json')) as f:
             self.permitted_roles = json.load(f)[__name__.split('.')[-1]]
 
@@ -30,10 +30,13 @@ class insults:
             return False
         return any(role in self.permitted_roles for role in user_roles)
 
-    @commands.command()
+    @commands.command(name='insult',
+                      brief='Make felix post a random insult',
+                      description='Make felix post a random insult',
+                      hidden=True,)
     @is_cc_channel()
     async def insult(self, ctx):
         await ctx.send(random.choice(questions))
 
-def setup(bot):
-    bot.add_cog(insults(bot))
+def setup(client):
+    client.add_cog(Insults(client))
