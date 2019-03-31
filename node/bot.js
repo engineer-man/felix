bcb7df0fe7b27c1040f465c08108a6f3a66bc83b
brtwrst <6873862+brtwrst@users.noreply.github.com>
2019-03-31 10:50:05 +0200

    Cleanup node side.
    Removed hangman, gif, google commands

--- node/bot.js
+++ node/bot.js
@@ -13,8 +13,6 @@ var state = {
     hangman_solver: false
 };
 
-//var dictionary = fs.readFileSync('/usr/share/dict/lwords').toString().split('\n');
-
 var handlers = {
 
     state_change(message) {
@@ -99,138 +97,6 @@ var handlers = {
             message.reply(msg)
         }
     },
-
-    hangman(message) {
-        const content = message.content;
-        const channel = message.channel;
-
-        var description = message.embeds[0].description;
-        var word_row = ' ' + description
-            .split('\n')
-            .filter(r => r.match(/^your word is/gi))[0]
-            .replace('your word is:', '')
-            .replace(/\\/gi, '')
-            .trim();
-
-        var incorrect = description
-            .split('\n')
-            .filter(r => r.trim().match(/^incorrect/gi));
-
-        if (incorrect.length > 0) {
-            incorrect = incorrect[0]
-                .split(':')[1]
-                .trim()
-                .split('');
-        }
-
-        var correct = description
-            .split('\n')
-            .filter(r => r.trim().match(/^correct/gi));
-
-        if (correct.length > 0) {
-            correct = correct[0]
-                .split(':')[1]
-                .trim()
-                .split('');
-        }
-
-        var word_len = 0;
-        var word_map = [];
-        var missing_idx = [];
-
-        for (var i = 0; i < word_row.length; ++i) {
-            if (word_row[i] === ' ') {
-                ++word_len;
-                ++i;
-                word_map.push(word_row[i]);
-                if (word_row[i] === '_') {
-                    missing_idx.push(word_map.length-1);
-                }
-            }
-        }
-
-        var valid_words = [];
-
-        dictionary.for_each(word => {
-            if (word.length !== word_len) return null;
-            if (word.match(/[0-9\-]+/gi)) return null;
-            if (~incorrect.index_of(word)) return null;
-
-            for (var i = 0; i < word_map.length; ++i) {
-                if (word_map[i] === '_') continue;
-                if (word[i] !== word_map[i]) return null;
-            }
-
-            valid_words.push(word);
-        });
-
-        var letters = {
-            a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0,
-            i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0,
-            q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0,
-            y: 0, z: 0,
-        };
-
-        for (var x = 0; x < valid_words.length; ++x) {
-            for (var i = 0; i < valid_words[x].length; ++i) {
-                ++letters[valid_words[x][i]];
-            }
-        }
-
-        incorrect.for_each(c => delete letters[c]);
-        correct.for_each(c => delete letters[c]);
-
-        var letter = null;
-        var letter_c = -1;
-
-        Object.keys(letters).for_each(key => {
-            if (letters[key] < letter_c || !key.match(/[a-z\-]/gi)) return null;
-            letter = key;
-            letter_c = letters[key];
-        });
-
-        channel.send('~ letter ' + letter);
-    },
-
-    // gif(message) {
-    //     const content = message.content;
-    //     const channel = message.channel;
-
-    //     message.reply('coming right up boss!');
-
-    //     var text = content.split('gif')[1].trim();
-
-    //     return request
-    //         ({
-    //             method: 'get',
-    //             url:
-    //                 'https://api.giphy.com/v1/gifs/search' +
-    //                 '?api_key=' + config.giphy_key +
-    //                 '&q=' + text.split(' ').join('+') +
-    //                 '&limit=20' +
-    //                 '&offset=0' +
-    //                 '&rating=R' +
-    //                 '&lang=en',
-    //             json: true,
-    //             simple: true
-    //         })
-    //         .then(res => {
-    //             var data = res.data;
-
-    //             if (data.length <= 0) {
-    //                 // no gif
-    //                 channel.send('sorry, no gif found for: ' + text);
-    //                 return null;
-    //             }
-
-    //             var gif = data[Math.floor(Math.random() * data.length)].images.original.url;
-
-    //             channel.send(gif);
-    //         })
-    //         .catch(err => {
-    //             channel.send('sorry, you broke me, no gifs right now :(');
-    //         });
-    // },
 
     code(message) {
         const content = message.content;
@@ -328,23 +194,9 @@ return bot
             return handlers.state_change(message);
         }
 
-        if (state.hangman_solver && message.embeds.length > 0 && ~message.embeds[0].description.indexOf('your word')) {
-            return handlers.hangman(message);
-        }
-
-        // if (content.match(/^felix gif /gi)) {
-        //     return handlers.gif(message);
-        // }
-
         if (content.match(/^felix run (js|javascript|python(2|3)?|node|c|c\+\+|cpp|ruby|go|r|cs|csharp|php|c#|nasm|asm|java|swift|brainfuck|bf)/gi)) {
             return handlers.code(message);
         }
-
-        // if (content.match(/^felix google/gi)) {
-        //     var text = content.split('google')[1].trim();
-
-        //     channel.send('here you go! <https://www.google.com/search?q=' + encodeURIComponent(text) + '>');
-        // }
 
         if (content.match(/^(hi|what's up|yo|hey|hello) felix/gi)) {
             message.reply('hello!');
