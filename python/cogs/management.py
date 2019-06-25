0620592a0e930229ca3784c7b57716d85fcf50a3
Brian Seymour <imbrianseymour@gmail.com>
2019-06-25 10:17:45 -0500

    update welcome message

--- python/cogs/management.py
+++ python/cogs/management.py
@@ -55,7 +55,8 @@ class Management(commands.Cog, name='Management'):
         await self.client.main_guild.system_channel.send(
             f'Welcome to the Engineer Man Discord Server, {member.mention}\n'
             'I\'m Felix, the server smart assistant. You can learn more about '
-            'what I can do by saying `felix help`. '
+            'what I can do by saying `felix help`. If you want answers to frequently asked '
+            'questions about Engineer Man, say `felix faq`. '
             'You can view the server rules in <#484103976296644608>. '
             'Please be kind and decent to one another. '
             'Glad you\'re here!'
