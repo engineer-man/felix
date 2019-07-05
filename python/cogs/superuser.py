277c1e0cfc6b1a1c0d633b8231de7b6c98982875
brtwrst <6873862+brtwrst@users.noreply.github.com>
2019-07-05 13:34:41 +0200

    add edit command for superusers

--- python/cogs/superuser.py
+++ python/cogs/superuser.py
@@ -9,6 +9,7 @@ can run commands from this cog.
 """
 
 import subprocess
+from discord import File
 from discord.ext import commands
 
 
@@ -20,7 +21,7 @@ class Superuser(commands.Cog, name='Superuser'):
         return self.client.user_is_superuser(ctx.author)
 
     # ----------------------------------------------
-    # Function to pull the latest changes from github
+    # Command to pull the latest changes from github
     # ----------------------------------------------
     @commands.group(
         invoke_without_command=True,
@@ -45,14 +46,14 @@ class Superuser(commands.Cog, name='Superuser'):
             await ctx.send(str(e))
 
     # ----------------------------------------------
-    # Function to reset the repo to a previous commit
+    # Command to reset the repo to a previous commit
     # ----------------------------------------------
     @git.command(
         name='reset',
     )
     async def reset(self, ctx, n):
         """Reset repo to HEAD~[n]"""
-        if not n>0:
+        if not n > 0:
             raise commands.BadArgument('Please specify n>0')
         await ctx.trigger_typing()
         try:
@@ -61,6 +62,72 @@ class Superuser(commands.Cog, name='Superuser'):
             await ctx.send('```git\n' + output + '\n```')
         except Exception as e:
             await ctx.send(str(e))
+
+    # ----------------------------------------------
+    # Commands to edit something concerning the bot or server
+    # ----------------------------------------------
+
+    @commands.group(
+        invoke_without_command=True,
+        name='edit',
+        hidden=True,
+    )
+    async def edit(self, ctx):
+        """Commands to edit the client user & server"""
+        await ctx.send_help('edit')
+
+    @edit.command(
+        name='server_icon',
+        aliases=['server_avatar', 'guild_icon', 'guild_avatar']
+    )
+    async def change_server_icon(self, ctx, pic_url=None):
+        """Change the server icon to the attached picture or provided URL"""
+        await ctx.trigger_typing()
+        try:
+            if pic_url:
+                async with self.client.session.get(pic_url) as resp:
+                    if resp.status != 200:
+                        raise RuntimeError('Could not download file...')
+                    pic_bytes = await resp.read()
+            else:
+                attachments = ctx.message.attachments
+                if not len(attachments) == 1:
+                    raise RuntimeError(
+                        'Please attach the new icon or provide the URL'
+                    )
+                attachment = attachments[0]
+                pic_bytes = await attachment.read()
+            await ctx.guild.edit(icon=pic_bytes)
+            await ctx.send('Success')
+        except Exception as e:
+            await ctx.send(str(e))
+
+    @edit.command(
+        name='bot_icon',
+        aliases=['bot_avatar']
+    )
+    async def change_bot_icon(self, ctx, pic_url=None):
+        """Change the bot icon to the attached picture or provided URL"""
+        await ctx.trigger_typing()
+        try:
+            if pic_url:
+                async with self.client.session.get(pic_url) as resp:
+                    if resp.status != 200:
+                        raise RuntimeError('Could not download file...')
+                    pic_bytes = await resp.read()
+            else:
+                attachments = ctx.message.attachments
+                if not len(attachments) == 1:
+                    raise RuntimeError(
+                        'Please attach the new icon or provide the URL'
+                    )
+                attachment = attachments[0]
+                pic_bytes = await attachment.read()
+            await self.client.user.edit(avatar=pic_bytes)
+            await ctx.send('Success')
+        except Exception as e:
+            await ctx.send(str(e))
+
 
 def setup(client):
     client.add_cog(Superuser(client))
