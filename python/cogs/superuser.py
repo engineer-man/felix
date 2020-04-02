9fefd38dbfaab8c31a4f12ed445f3191fa169a8b
brtwrst <6873862+brtwrst@users.noreply.github.com>
2020-04-02 20:26:31 +0200

    cleanup

--- python/cogs/superuser.py
+++ python/cogs/superuser.py
@@ -2,7 +2,9 @@
 It will add some superuser commands to a bot.
 
 Commands:
-    pull       pull latest changes from github
+    git
+      pull       pull latest changes from github
+      reset      reset n commits
 
 Only users which are specified as a superuser in the config.json
 can run commands from this cog.
@@ -71,84 +73,6 @@ class Superuser(commands.Cog, name='Superuser'):
             await ctx.send('```git\n' + output + '\n```')
         except Exception as e:
             await ctx.send(str(e))
-
-    # ----------------------------------------------
-    # Commands to edit something concerning the bot or server
-    # ----------------------------------------------
-
-    @commands.group(
-        invoke_without_command=True,
-        name='edit',
-        hidden=True,
-    )
-    async def edit(self, ctx):
-        """Commands to edit the client user & server"""
-        await ctx.send_help('edit')
-
-    @edit.command(
-        name='server_icon',
-        aliases=['server_avatar', 'guild_icon', 'guild_avatar']
-    )
-    async def change_server_icon(self, ctx, pic_url=None):
-        """Change the server icon to the attached picture or provided URL"""
-        await ctx.trigger_typing()
-        try:
-            if pic_url:
-                async with self.client.session.get(pic_url) as resp:
-                    if resp.status != 200:
-                        raise RuntimeError('Could not download file...')
-                    pic_bytes = await resp.read()
-            else:
-                attachments = ctx.message.attachments
-                if not len(attachments) == 1:
-                    raise RuntimeError(
-                        'Please attach the new icon or provide the URL'
-                    )
-                attachment = attachments[0]
-                pic_bytes = await attachment.read()
-            await ctx.guild.edit(icon=pic_bytes)
-            await ctx.send('Success')
-        except Exception as e:
-            await ctx.send(str(e))
-
-    @edit.command(
-        name='bot_icon',
-        aliases=['bot_avatar']
-    )
-    async def change_bot_icon(self, ctx, pic_url=None):
-        """Change the bot icon to the attached picture or provided URL"""
-        await ctx.trigger_typing()
-        try:
-            if pic_url:
-                async with self.client.session.get(pic_url) as resp:
-                    if resp.status != 200:
-                        raise RuntimeError('Could not download file...')
-                    pic_bytes = await resp.read()
-            else:
-                attachments = ctx.message.attachments
-                if not len(attachments) == 1:
-                    raise RuntimeError(
-                        'Please attach the new icon or provide the URL'
-                    )
-                attachment = attachments[0]
-                pic_bytes = await attachment.read()
-            await self.client.user.edit(avatar=pic_bytes)
-            await ctx.send('Success')
-        except Exception as e:
-            await ctx.send(str(e))
-
-
-    @edit.command(
-        name='bot_name',
-    )
-    async def change_bot_name(self, ctx, name):
-        """Change the bot name"""
-        try:
-            await self.client.user.edit(username=name)
-            await ctx.send('Success')
-        except Exception as e:
-            await ctx.send(str(e))
-
 
 def setup(client):
     client.add_cog(Superuser(client))
