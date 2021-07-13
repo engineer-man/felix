f1b5ed2d9ebc48eb59ce14eab7631fc024cea1b2
brtwrst <6873862+brtwrst@users.noreply.github.com>
2021-07-13 06:30:48 +0000

    smarter inspect truncation

--- python/cogs/general.py
+++ python/cogs/general.py
@@ -659,8 +659,8 @@ class General(commands.Cog, name='General'):
         sauce = ''.join(saucelines)
         # Little hack so triple quotes don't end discord codeblocks when printed
         sanitized = sauce.replace('`', '\u200B`')
-        if len(sanitized) > 1900:
-            sanitized = sanitized[:1900] + '\n[...]'
+        if len(url) + len(sanitized) > 1950:
+            sanitized = sanitized[:1950-len(url)] + '\n[...]'
         await ctx.send(url + f'```python\n{sanitized}\n```')
 
     @commands.command(
