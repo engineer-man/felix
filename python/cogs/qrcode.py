2d385096786d54c326e8138f80c663d40dcf18e9
brtwrst <6873862+brtwrst@users.noreply.github.com>
2022-05-03 14:20:15 +0000

    fix bug with trailing backslash

--- python/cogs/qrcode.py
+++ python/cogs/qrcode.py
@@ -24,6 +24,8 @@ class QRCode(commands.Cog, name='QRCode'):
         2 : Q
         3 : H
         """
+        if level is None:
+            level = 0
         big_str, small_str = generate_qr_code(data, level, output='all', verbose=False)
         if len(big_str) > 1990 and len(small_str) > 1990:
             raise commands.BadArgument('QR Code too big, use less data or lower error correction')
