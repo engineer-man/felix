7ae5021fc06ceb09e0f6841b6d552b02db49689a
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-11-16 06:15:03 +0800

    add DATABASE_FILENAME, remove DATABASE_URL
    basically, refactor the way we supply the database url
    to create_async_engine

--- python/db/config.py
+++ python/db/config.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# We have a command in cogs/management.py that needs just the database
+# filename, which dumps the db
+DATABASE_FILENAME = 'felix.sqlite'
 
-DATABASE_URL = 'sqlite+aiosqlite:///felix.sqlite'
-
-engine = create_async_engine(DATABASE_URL, future=True, echo=False)
+engine = create_async_engine('sqlite+aiosqlite:///' + DATABASE_FILENAME, future=True, echo=False)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
